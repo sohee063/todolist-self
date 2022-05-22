35c11f968a3967c86b66445b527da13922be28ea
sohee kim <sohee063@nate.com>
2022-05-22 15:16:33 +0900

    모두진행중끝남 메뉴에서 삭제버튼 클릭하면 실시간 ui적용

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// 1. 입력창에 할일을 입력하고 +버튼을 누르면 할일 목록에 추가가 된다.
-// 2. delete버튼을 누르면 할일이 리스트에서 삭제가 된다.
-// 3. check버튼을 누르면 할일이 끝난 것으로 간주하고 밑줄이 간다. check 버튼이 되돌리기 버튼으로 변경 된다.
-// 4. 끝난 할일은 되돌리기 버튼을 클릭하면 다시 되돌릴 수 있다.
-// 5. 진행중/끝남 상태로 나누어서 볼 수 있다.
-// 6. 모바일 반응형 웹이다.
-
 let addButton = document.getElementById("add-button");
 let taskInput = document.getElementById("task-input");
 let tabs = document.querySelectorAll(".task-tabs div");
@@ -69,6 +62,8 @@ function toggleComplete(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id === id) {
       taskList[i].isComplete = !taskList[i].isComplete;
+      filterList.splice(i, 1);
+      render();
       break;
     }
   }
@@ -80,6 +75,8 @@ function deleteTask(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id === id) {
       taskList.splice(i, 1);
+      filterList.splice(i, 1);
+      render();
     }
   }
   render();
